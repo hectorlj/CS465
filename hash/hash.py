9c2d832feaffed8bb371f23f0fcca286e7183fbe
Hector Lopez <hlj239@byu.edu>
2019-09-26 00:32:57 -0600

    hash

---
+++ hash/hash.py
@@ -0,0 +1,60 @@
+import hashlib
+import string
+import random as rand 
+
+# time = 2**(n/2) n = bits
+def collision (n):
+    temp = string.hexdigits
+    count = ''.join(rand.choice(temp) for i in range(20))
+    hashSet = set()
+    counter = -1
+    while 1:
+        newhash = hash(count, n)
+        count = str(hex(int(count, 16) + 1))
+        counter = counter + 1
+        if newhash in set(hashSet):
+            return counter
+        hashSet.add(newhash)
+
+# time = 2**n n = bits
+def preimage (hashString, n):
+    prehash = hash(hashString, n)
+    temp = string.hexdigits
+    count = ''.join(rand.choice(temp) for i in range(10))
+    newHash = ''
+    counter = 0
+    while prehash != newHash:
+        newHash = hash(count, n)
+        count = str(hex(int(count, 16) + 1))
+        counter = counter + 1
+    return counter
+
+def hash(myString, n):
+    myString = bytes(myString, 'utf-8')
+    hash = hashlib.sha1(myString).hexdigest()
+    binaryHash = bin(int(hash, 16))[2:]
+    shortHash = binaryHash[0:n + 1]
+    return shortHash
+
+n = 8
+expectedPreimage = 2**n
+expectedCollision = 2**(n/2)
+totalTrials = 100
+currTrial = 0
+x = 0
+average = 0
+while x < totalTrials:
+    currTrial = preimage("hello there", n)
+    average = average + currTrial
+    x = x + 1
+print("n = ", str(n), "\n Average preimage: ", str(average/totalTrials), " \n Expected preimage: ", str(expectedPreimage))
+
+average = 0
+currTrial = 0
+x = 0
+
+while x < totalTrials:
+    currTrial = collision(n)
+    average = average + currTrial
+    x = x + 1
+print("n = ", str(n), "\n Average collision: ", str(average/totalTrials), " \n Expected collision: ", str(expectedCollision))
